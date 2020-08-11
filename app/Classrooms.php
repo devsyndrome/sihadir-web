84e090be12929539503745b5215db3e27eab364f
arbians <devsyndrome@gmail.com>
2020-08-11 14:30:58 +0700

    add CRUD Classrooms

--- app/Classrooms.php
+++ app/Classrooms.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Classrooms extends Model
 {
-    //
+    protected $guarded = [];  
+    public $primaryKey = 'classroom_id';
 }
--- app/Http/Controllers/ClassroomsController.php
+++ app/Http/Controllers/ClassroomsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Classrooms;
+use Illuminate\Support\Facades\Hash;
 
 class ClassroomsController extends Controller
 {
@@ -11,8 +13,21 @@ class ClassroomsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $list_classrooms = classrooms::all();
+        if($request->ajax()){
+            return datatables()->of($list_classrooms)
+            ->addColumn('action', function($data){
+                $button = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$data->classroom_id.'" data-original-title="Edit" class="edit btn btn-info btn-sm edit-post"><i class="far fa-edit"></i></a>';
+                $button .= '&nbsp;&nbsp;';
+                $button .= '<button type="button" name="delete" id="'.$data->classroom_id.'" class="delete btn btn-danger btn-sm"><i class="far fa-trash-alt"></i></button>';     
+                return $button;
+            })
+            ->rawColumns(['action'])
+            ->addIndexColumn()
+            ->make(true);
+        }
         return view('classrooms');
     }
 
@@ -32,9 +47,15 @@ class ClassroomsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
     public function store(Request $request)
     {
-        //
+        
+        $id = $request->id;
+        $post = classrooms::updateOrCreate(['classroom_id' => $request->id],
+            [
+                'classroom_name' => $request->name
+            ]);
     }
 
     /**
@@ -56,7 +77,9 @@ class ClassroomsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $where = array('classroom_id' => $id);
+        $post  = classrooms::where($where)->first();
+        return response()->json($post);
     }
 
     /**
@@ -79,6 +102,7 @@ class ClassroomsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = classrooms::where('classroom_id',$id)->delete();
+        return response()->json($post);
     }
 }
